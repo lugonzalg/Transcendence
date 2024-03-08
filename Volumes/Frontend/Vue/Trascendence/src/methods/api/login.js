7a7e5a597aa612dbb72cf9e708c1e6a5f35eb8c2
root <root@srv458138.hstgr.cloud>
2024-03-08 17:50:51 +0000

    fix rutas

--- Volumes/Frontend/Vue/Trascendence/src/methods/api/login.js
+++ Volumes/Frontend/Vue/Trascendence/src/methods/api/login.js
@@ -19,7 +19,7 @@ function collectBrowserData() {
 
 //HomeView @POST /login_log
 async function sendDataToServer(data) {
-    axiosInstance.post('http://localhost:4242/api/log', data,  { timeout: 5000 })
+    axiosInstance.post('https://trascendende.tech/api/login/log', data,  { timeout: 5000 })
     .then((response) => {
       console.log('Datos enviados al servidor:', response);
     })
@@ -58,7 +58,7 @@ async function handleIntraRedirect() {
       // Peticion de build url al back
      // const response = await axios.get('http://localhost:4242/api/intra');
       // Redireccion a la url
-      window.location.href = 'http://localhost:4242/api/intra';
+      window.location.href = 'https://trascendence.tech/api/intra';
     
     } catch (error) {
       console.error('Error al manejar el inicio de sesión con Intra 42:', error);
