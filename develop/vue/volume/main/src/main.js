30912fd608e97c3464a6788631b8f07c16c8b8e7
root <root@srv458138.hstgr.cloud>
2024-03-12 20:34:08 +0000

    curls al vault

--- develop/vue/volume/main/src/main.js
+++ develop/vue/volume/main/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import VueCookies from 'vue-cookies';
+import secrets from './../secrets.json'; 
 
 /*
     Inicialización de la aplicación Vue 2-3
--- develop/vue/volume/main/vue.config.js
+++ develop/vue/volume/main/vue.config.js
@@ -1,26 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
-  configureWebpack:{
-    plugins:
-    [
+  configureWebpack: {
+    plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-      })
+      }),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.json$/,
+          loader: 'json-loader',
+          type: 'javascript/auto',
+        },
+      ],
+    },
   },
-}
-),
-{
   css: {
     loaderOptions: {
       sass: {
-        implementation: require('sass'), // Para Sass
-        // implementation: require('node-sass'), // Para SCSS
+        implementation: require('sass'),
       },
     },
   },
-}; 
-
+};
