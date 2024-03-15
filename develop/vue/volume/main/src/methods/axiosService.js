ec5e3b8106432cd0c11e4ad9462fb23f417ae929
lugarte <lugarte>
2024-03-15 09:11:05 +0000

    ONGO: creating path for redirect uri

--- develop/vue/volume/main/src/methods/axiosService.js
+++ develop/vue/volume/main/src/methods/axiosService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 
-    baseURL: 'http://localhost:4242/api/',
+    baseURL: 'http://gateway:4242/api/',
     timeout: 1000 // Timeout porsiaca ya haremos pruebas
     //headers: {'X-Custom-Header': 'foobar'} Aqui meteremos el bearer por ejemplo
 });
