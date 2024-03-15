dd0050f7bcebef476ed968998ef4dd765941eabe
lugarte <lugarte>
2024-03-15 13:01:09 +0000

    CHORE: log endpoint modified to gateway from localhost

--- develop/vue/volume/main/src/methods/api/login.js
+++ develop/vue/volume/main/src/methods/api/login.js
@@ -19,7 +19,7 @@ function collectBrowserData() {
 
 //HomeView @POST /login_log
 async function sendDataToServer(data) {
-    axiosInstance.post('http://localhost:4242/api/log', data,  { timeout: 5000 })
+    axiosInstance.post('http://gateway:4242/api/log', data,  { timeout: 5000 })
     .then((response) => {
       console.log('Datos enviados al servidor:', response);
     })
